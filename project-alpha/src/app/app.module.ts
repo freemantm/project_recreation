98ab50c216f51fc8339a0defe55ed2bc89b5226a
Timothy Freeman <freemantimothym@gmail.com>
2019-03-12 13:40:10 -0400

    add basic components templates and ng bootstrap

--- project-alpha/src/app/app.module.ts
+++ project-alpha/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
